6ca7b779f81d87a647ff627003c35967683967bc
Ivnlesko <il.ivanlesko@example.com>
2021-05-04 15:30:58 +0200

    DD done?

--- JS/dropdown.js
+++ JS/dropdown.js
@@ -6,44 +6,128 @@ var ddShop = document.getElementsByClassName("ddShop");
 
 document
   .querySelector("#navShop")
-  .addEventListener("mouseenter", dropdownEnter);
-document
-  .querySelector("#navShop")
-  .addEventListener("mouseleave", dropdownLeave);
+  .addEventListener("mouseenter", dropdownEnterS);
 
-function dropdownEnter() {
+document
+  .querySelector("#liShop")
+  .addEventListener("mouseenter", dropdownEnterS);
+
+function dropdownEnterS() {
+  console.log("I do work");
+  //Show dropdown
   dropdown.classList.remove("hidden");
+  //Hide everything else
+  dropdownCont.classList.remove("dropdAbout");
+  document.querySelector(".ddAbout").classList.add("hidden");
+  //remove yellow from rest
+  document.querySelector("#liAboutus").style.color = "white";
+  document.querySelector("#liAboutus").style.fontWeight = "normal";
+  document.querySelector("#liSale").style.color = "white";
+  document.querySelector("#liSale").style.fontWeight = "normal";
+  document.querySelector("#liNA").style.color = "white";
+  document.querySelector("#liNA").style.fontWeight = "normal";
+  //Show the related stuff
   document.querySelector("#firstUl").classList.remove("hidden");
   document.querySelector("#secondUl").classList.remove("hidden");
   document.querySelector("#thirdUl").classList.remove("hidden");
+  //Add related class (for correct layout)
   dropdownCont.classList.add("dropdShop");
+  //Make the heading yellow
+  document.querySelector("#liShop").style.color = "#EEEE9E";
+  document.querySelector("#liShop").style.fontWeight = "bold";
 }
 
-function dropdownLeave() {
-  console.log("I am connected");
-  dropdown.classList.add("hidden");
-  dropdownCont.classList.remove("dropdShop");
+document.querySelector("#liNA").addEventListener("mouseenter", dropdownEnterNA);
+
+function dropdownEnterNA() {
+  console.log("I do work");
+  //Show dropdown
+  dropdown.classList.remove("hidden");
+  //Hide everything else
+  dropdownCont.classList.remove("dropdAbout");
+  document.querySelector(".ddAbout").classList.add("hidden");
   document.querySelector("#firstUl").classList.add("hidden");
   document.querySelector("#secondUl").classList.add("hidden");
   document.querySelector("#thirdUl").classList.add("hidden");
+  //remove yellow from rest
+  document.querySelector("#liAboutus").style.color = "white";
+  document.querySelector("#liAboutus").style.fontWeight = "normal";
+  document.querySelector("#liShop").style.color = "white";
+  document.querySelector("#liShop").style.fontWeight = "normal";
+  document.querySelector("#liSale").style.color = "white";
+  document.querySelector("#liSale").style.fontWeight = "normal";
+
+  //Show the related stuff
+  //Add related class (for correct layout)
+  //Make the heading yellow
+  document.querySelector("#liNA").style.color = "#EEEE9E";
+  document.querySelector("#liNA").style.fontWeight = "bold";
+}
+
+document
+  .querySelector("#liSale")
+  .addEventListener("mouseenter", dropdownEnterSale);
+
+function dropdownEnterSale() {
+  console.log("I do work");
+  //Show dropdown
+  dropdown.classList.remove("hidden");
+  //Hide everything else
+  dropdownCont.classList.remove("dropdAbout");
+  document.querySelector(".ddAbout").classList.add("hidden");
+  document.querySelector("#firstUl").classList.add("hidden");
+  document.querySelector("#secondUl").classList.add("hidden");
+  document.querySelector("#thirdUl").classList.add("hidden");
+  //remove yellow from rest
+  document.querySelector("#liAboutus").style.color = "white";
+  document.querySelector("#liAboutus").style.fontWeight = "normal";
+  document.querySelector("#liShop").style.color = "white";
+  document.querySelector("#liShop").style.fontWeight = "normal";
+  document.querySelector("#liNA").style.color = "white";
+  document.querySelector("#liNA").style.fontWeight = "normal";
+  //Show the related stuff
+  //Add related class (for correct layout)
+  //Make the heading yellow
+  document.querySelector("#liSale").style.color = "#EEEE9E";
+  document.querySelector("#liSale").style.fontWeight = "bold";
 }
 
 document
   .querySelector("#navAboutus")
   .addEventListener("mouseenter", dropdownEnterA);
+
 document
-  .querySelector("#navAboutus")
-  .addEventListener("mouseleave", dropdownLeaveA);
+  .querySelector("#liAboutus")
+  .addEventListener("mouseenter", dropdownEnterA);
 
 function dropdownEnterA() {
+  //Show dropdown
   dropdown.classList.remove("hidden");
+  //Hide everything else
+  document.querySelector("#firstUl").classList.add("hidden");
+  document.querySelector("#secondUl").classList.add("hidden");
+  document.querySelector("#thirdUl").classList.add("hidden");
+  //remove yellow from rest
+  document.querySelector("#liShop").style.color = "white";
+  document.querySelector("#liShop").style.fontWeight = "normal";
+  document.querySelector("#liNA").style.color = "white";
+  document.querySelector("#liNA").style.fontWeight = "normal";
+  document.querySelector("#liSale").style.color = "white";
+  document.querySelector("#liSale").style.fontWeight = "normal";
+  //Show the related stuff
   document.querySelector(".ddAbout").classList.remove("hidden");
+  //Add related class (for correct layout)
   dropdownCont.classList.add("dropdAbout");
+  //Add yellow
+  document.querySelector("#liAboutus").style.color = "#EEEE9E";
+  document.querySelector("#liAboutus").style.fontWeight = "bold";
 }
 
-function dropdownLeaveA() {
+document
+  .querySelector(".dropdown")
+  .addEventListener("mouseleave", dropdownLeave);
+
+function dropdownLeave() {
   console.log("I am connected");
   dropdown.classList.add("hidden");
-  dropdownCont.classList.remove("dropdAbout");
-  document.querySelector(".ddAbout").classList.add("hidden");
 }
