a7604de77eba66df576ed006bc88e2d0ecd84470
Ivnlesko <il.ivanlesko@example.com>
2021-05-04 14:19:43 +0200

    dropdown - shop, about working

--- JS/dropdown.js
+++ JS/dropdown.js
@@ -2,7 +2,7 @@ const dropdown = document.querySelector(".dropdown");
 let dropdownDown = false;
 const dropdownCont = document.querySelector("#ddCategories");
 const ddAbout = document.querySelector(".ddAbout");
-const ddShop = document.querySelector(".ddShop");
+var ddShop = document.getElementsByClassName("ddShop");
 
 document
   .querySelector("#navShop")
@@ -12,29 +12,38 @@ document
   .addEventListener("mouseleave", dropdownLeave);
 
 function dropdownEnter() {
-  if (dropdownDown == true) {
-    console.log("I am connected");
-    dropdown.classList.add("hidden");
-    dropdownCont.classList.remove("dropdShop");
-
-    dropdownDown = false;
-  } else {
-    dropdown.classList.remove("hidden");
-    ddAbout.classList.add("hidden");
-
-    dropdownCont.classList.add("dropdShop");
-
-    dropdownDown = true;
-  }
+  dropdown.classList.remove("hidden");
+  document.querySelector("#firstUl").classList.remove("hidden");
+  document.querySelector("#secondUl").classList.remove("hidden");
+  document.querySelector("#thirdUl").classList.remove("hidden");
+  dropdownCont.classList.add("dropdShop");
 }
 
 function dropdownLeave() {
-  if (dropdownDown == true) {
-    console.log("I am connected");
-    dropdown.classList.add("hidden");
-    dropdownDown = false;
-  } else {
-    dropdown.classList.remove("hidden");
-    dropdownDown = true;
-  }
+  console.log("I am connected");
+  dropdown.classList.add("hidden");
+  dropdownCont.classList.remove("dropdShop");
+  document.querySelector("#firstUl").classList.add("hidden");
+  document.querySelector("#secondUl").classList.add("hidden");
+  document.querySelector("#thirdUl").classList.add("hidden");
+}
+
+document
+  .querySelector("#navAboutus")
+  .addEventListener("mouseenter", dropdownEnterA);
+document
+  .querySelector("#navAboutus")
+  .addEventListener("mouseleave", dropdownLeaveA);
+
+function dropdownEnterA() {
+  dropdown.classList.remove("hidden");
+  document.querySelector(".ddAbout").classList.remove("hidden");
+  dropdownCont.classList.add("dropdAbout");
+}
+
+function dropdownLeaveA() {
+  console.log("I am connected");
+  dropdown.classList.add("hidden");
+  dropdownCont.classList.remove("dropdAbout");
+  document.querySelector(".ddAbout").classList.add("hidden");
 }
