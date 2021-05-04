4cf7d83a540c2cf76ddac573cba812638bd344ca
Ivnlesko <il.ivanlesko@example.com>
2021-05-04 13:05:32 +0200

    DD shop - working

--- JS/dropdown.js
+++ JS/dropdown.js
@@ -1,5 +1,9 @@
 const dropdown = document.querySelector(".dropdown");
 let dropdownDown = false;
+const dropdownCont = document.querySelector("#ddCategories");
+const ddAbout = document.querySelector(".ddAbout");
+const ddShop = document.querySelector(".ddShop");
+
 document
   .querySelector("#navShop")
   .addEventListener("mouseenter", dropdownEnter);
@@ -11,9 +15,15 @@ function dropdownEnter() {
   if (dropdownDown == true) {
     console.log("I am connected");
     dropdown.classList.add("hidden");
+    dropdownCont.classList.remove("dropdShop");
+
     dropdownDown = false;
   } else {
     dropdown.classList.remove("hidden");
+    ddAbout.classList.add("hidden");
+
+    dropdownCont.classList.add("dropdShop");
+
     dropdownDown = true;
   }
 }
